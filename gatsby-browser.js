3f12ade7f804007edabca2efb3f67c43d59406d1
John Ochs <code@johnochs.io>
2023-08-24 12:47:22 -0700

    Gatsby Configuration files.

---
+++ gatsby-browser.js
@@ -0,0 +1,1 @@
+import "./src/gatsby-theme-portfolio-minimal/globalStyles/theme.css";
--- gatsby-config.js
+++ gatsby-config.js
@@ -14,15 +14,41 @@ module.exports = {
           display: "minimal-ui", // Used in manifest.json
         },
         contentDirectory: "./content",
-        blogSettings: {
-          path: "/blog", // Defines the slug for the blog listing page
-          usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
-        },
-        // googleAnalytics: {
+        // blogSettings: {
+        //   entityName: "",
+        //   path: "", // Defines the slug for the blog listing page
+        //   usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article
+        // },
+        //// googleAnalytics: {
         //     trackingId: "UA-XXXXXX-X",
         //     anonymize: true, // Default true
         //     environments: ["production", "development"] // Default ["production"]
         // }
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Open Sans`,
+            file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
+          },
+          {
+            name: `Slackside One`,
+            file: `https://fonts.googleapis.com/css2?family=Slackside+One&display=swap`,
+          },
+          {
+            name: `La Belle Aurore`,
+            file: `https://fonts.googleapis.com/css2?family=La+Belle+Aurore&display=swap`,
+          },
+        ],
       },
     },
   ],
