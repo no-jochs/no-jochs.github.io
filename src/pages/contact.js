57110f1c3e9c8002029791a1fe2421fca984208a
John Ochs <code@johnochs.io>
2023-11-20 14:03:51 -0800

    New contact page.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Page } from "gatsby-theme-portfolio-minimal";
-import { ContactSection } from "../gatsby-theme-portfolio-minimal/sections/Contact";
+import { Page, ContactSection } from "gatsby-theme-portfolio-minimal";
 import Layout from "../components/layout";
 
 export default function ContactPage() {
